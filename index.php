93f3490bfcec0b579d5a8c254b7e8935720853a3
AmujoDotun <samuelamujo@gmail.com>
2020-05-05 12:42:09 +0100

    added form to the index form

--- index.php
+++ index.php
@@ -4,16 +4,17 @@
 require 'core/boostrap.php';
 
 
+require Router::load('routes.php')
+->direct(Request::uri(), Request::method());
+
+// $router = new Router;
+
+// require 'routes.php';
+
+// // parse_url($_REQUEST[$_REQUEST]);
+
+// $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+// // trim($_SERVER['REQUEST_URI'], '/');
 
 
-$router = new Router;
-
-require 'routes.php';
-
-// parse_url($_REQUEST[$_REQUEST]);
-
-$uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
-// trim($_SERVER['REQUEST_URI'], '/');
-
-
-require $router->direct($uri);
+// require $router->direct($uri);
